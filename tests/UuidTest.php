79cae13382d001c4f0172c12cf5594ea7a858d29
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-11-12 11:42:45 +0100

    Fix unit test

--- tests/UuidTest.php
+++ tests/UuidTest.php
@@ -19,6 +19,6 @@ class UuidTest
 
 	function testGeneratev5()
 	{
-		Nose::assertEquals(UUID::v5("Hello, world!")->toString(), "a36e854defad58cdbd0084259b83901d");
+		Nose::assertEquals(UUID::v5("Hello, world!")->toString(), "9112042407e0506ca6a4726e89871b72");
 	}
 }
